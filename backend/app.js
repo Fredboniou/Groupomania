274bd82009700db5b188269b87d74a0cddd0d21e
Fred Boniou <f.boniou@gmail.com>
2021-12-08 12:12:22 +0100

    middleware auth et multer

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,16 @@ const app = express();
 
 app.use(express.json()); //remplace body-parser devenu obsolète
 
+const userRoutes = require("./routes/userRoutes");
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+
+  app.use("api/user", userRoutes);
 
 
 module.exports = app;
