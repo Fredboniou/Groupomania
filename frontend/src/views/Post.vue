8b1764286b53a6cb6973ac2533bfa9c3c8ec46a9
Fred Boniou <f.boniou@gmail.com>
2021-12-15 19:04:37 +0100

    correction authentification

--- frontend/src/views/Post.vue
+++ frontend/src/views/Post.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="post">
-        <h3>Tous les posts</h3>
-        <button @click="getAllPosts">voir les posts</button>
+    <div class="noPost" v-if="this.posts = []">
+        <h1>Aucun post à afficher</h1>
+    </div>
+    <div class="post" v-else>
+        <h3>Fil d'actualité</h3>
     </div>
 </template>
 
@@ -10,22 +12,32 @@ import axios from "axios";
 
 export default {
     name: "Post",
+    data() {
+        return {
+            posts: [],
+        }
+    },
     
-    mounted() {
+    mounted: function() {
         if(localStorage.getItem("form")) {
             try {
-                this.form = JSON.parse(localStorage.getItem("form"))
+                this.getAllPosts()
             } catch(error) {
                 localStorage.removeItem("form");
+                this.$router.push("/")
             }
+        } else {
+            this.$router.push("/")
         }
     },
     methods: {
         async getAllPosts() {
             const data = JSON.parse(localStorage.getItem("form"));
             const token = data.token;
+
             axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-            axios.get("http://localhost:3000/api/post", {
+
+            axios.get("http://localhost:3000/api/post/", {
                 headers: {
                     Authorization: "bearer " + token
                 },
@@ -42,5 +54,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+div{
+    .post, .noPost {
+        margin: auto;
+        width: 50%;
+        min-width: 380px;
+        max-width: 600px;
+        background: white;       
+        padding: 60px 45px 30px;
+        border-radius: 16px;
+        box-shadow: 0 1px 4px rgba(0, 0, 0, 0.05);
+        position: relative;
+    }
+    .post {
+        display: grid;
+        grid-template-rows: repeat(4, 104px);
+    }
+}
 </style>
