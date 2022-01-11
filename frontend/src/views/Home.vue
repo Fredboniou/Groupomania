b8b26adffffa525c0f2a5f0b8ab062832d8d5062
Fred Boniou <f.boniou@gmail.com>
2022-01-11 19:00:56 +0100

    responsive page home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="home">
     <div id="nav">
-      <router-link to="/" aria-label="Page d'accueil">Home</router-link> |
-      <router-link to="/about" aria-label="A propos de notre société">About</router-link> |
-      <router-link to="/login" aria-label="Accès membre">Accès membre</router-link> |
-      <router-link to="/signup" aria-label="Créer un compte">Créer un compte</router-link>
+      <input type="checkbox" id="menu-checkbox" class="menu-checkbox" />
+      <label for="menu-checkbox" class="menu-toggle">&equiv; Menu</label>
+      <ul class="menu">
+        <li><router-link to="/" aria-label="Page d'accueil">Home</router-link></li><span> | </span>
+        <li><router-link to="/about" aria-label="A propos de notre société">About</router-link></li><span> | </span>
+        <li><router-link to="/login" aria-label="Accès membre">Accès membre</router-link></li><span> | </span>
+        <li><router-link to="/signup" aria-label="Créer un compte">Créer un compte</router-link></li>
+      </ul>
     </div>
     <img alt="logo" src="../assets/icone-black-rs.png">
     <HomeComponents msg="Bienvenue sur le réseau social Groupomania, créé pour vous, avec vous !"/>
@@ -24,32 +28,70 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+li {
+  list-style: none;
+}
+.menu {
+  display: flex;
+  justify-content: space-around;
+}
+.menu-checkbox {
+  //visibility: hidden;
+  display: none;
+}
+.menu-toggle {
+  display: none;
+}
 
-  #nav {
+#nav {
   padding: 30px;
   margin-bottom: -30px;
-
-
   a {
     text-decoration: none;
     font-weight: bold;
     color: #000;
-
     &:hover {
       color: #fc2e06
     }
   }
 }
-
 img {
   width: 500px;
   height: 500px;
 }
-
-@media all and (max-width : 480px) {
+@media all and (max-device-width : 480px) {
   img {
     width: 300px;
     height: 300px;
+  }
+  .menu-checkbox {
+    display: initial;
+    opacity: 0;
+    position: absolute;
+    top: -1000px;
+  }
+  .menu {
+    display: none;
+  }
+  .menu-checkbox:checked ~ .menu {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+  span {
+    display: none;
+  }
+  .menu-checkbox:focus + .menu-toggle {
+    outline: #fc2e06 auto 5px;
+  }
+  .menu-toggle {
+    display: initial;
+    padding: .5em 1em;
+  }
+  #nav {
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap; 
   }
 }
 </style>
